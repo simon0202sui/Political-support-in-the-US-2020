35bd639c11a4f1ac1c107949ddd034ae4244a7c4
Xuecheng Gao <ggggkasper@gmail.com>
2024-03-15 03:20:28 -0400

    paper

---
+++ paper/paper.qmd.R
@@ -0,0 +1,33 @@
+---
+  title: "support-in-the-US-2020"
+author: 
+- Dai Lin
+- Pengyu Sui
+- Xucheng Gao
+thanks: "Code and data are available at: "https://github.com/simon0202sui/Political-support-in-the-US-2020"
+date: today
+date-format: long
+abstract: "First sentence. Second sentence. Third sentence. Fourth sentence."
+format: pdf
+number-sections: true
+bibliography: references.bib
+---
+  
+  ```{r}
+#| include: false
+#| warning: false
+#| message: false
+
+library(tidyverse)
+library(palmerpenguins)
+library(ggrepel)
+library(maps)
+library(here)
+library(kableExtra)
+library(statebins)
+library(broom.mixed)
+library(knitr)
+
+#### Read in necessary data and model ####
+
+```
