61c21e8fac178ffd3884ad17e44bd603ccfff3a3
trainingwolf8878 <dailin20000730@gmail.com>
2024-03-17 02:09:58 +0000

    e1

--- script/02_clean_data_graph.R
+++ script/02_clean_data_graph.R
@@ -1,6 +1,5 @@
 #load work space
-cleaned_data_survey <- read_csv("termpaper 3 rstuido workspace/old/inputs/data_cleaned(1).csv")
-
+cleaned_data_survey <- read.csv("outputs/clean_data/data_cleaned(1).csv")
 
 # Visualization 1: Support for Biden Trend by Age Group
 ggplot(cleaned_data_survey, aes(x = age_group, y = ..count.., group = support_biden, color = support_biden)) +
@@ -55,12 +54,13 @@ ggplot(cleaned_data_survey, aes(x = gender, fill = support_biden)) +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 0, hjust = 0.5))
 
-ggplot(cleaned_data_survey, aes(x = race, y = support_biden, fill = support_biden)) +
+ggplot(cleaned_data_survey, aes(x = gender, y = support_biden, fill = support_biden)) +
   geom_jitter(position = position_jitter(width = 0.2), size = 3, color = "black") +
   scale_fill_manual(values = c('Yes' = 'blue', 'No' = 'red')) +
-  labs(title = "Support for Biden by Race", x = "Race", y = "Support for Biden") +
+  labs(title = "Support for Biden by Gender", x = "Gender", y = "Support for Biden") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1))
+
 
 
